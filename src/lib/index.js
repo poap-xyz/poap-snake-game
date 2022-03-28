c6d5b4ce6182b5e58357edfc3f30ab2df78d3c99
seb <varseb@gmail.com>
2022-03-28 16:37:18 -0300

    remove unused helpers

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,18 +1,4 @@
 
-
-export function getDistance(x1, y1, x2, y2){
-  const dy = x2 - x1
-  const dx = y2 - y1
-
-  return Math.sqrt(dx * dx + dy * dy)
-}
-
-
-export function circleCollision(p1x, p1y, r1, p2x, p2y, r2) {
-  return r1 + r2 > getDistance(p1x, p1y, p2x, p2y)
-}
-
-
 export function getRandomInt(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
